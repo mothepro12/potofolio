4cf51f0b05f5f62420df0ba97a0df4b55ded5693
mothepro12 <mohamedalaazx@gmail.com>
2023-12-22 04:26:43 -0500

    Added

---
+++ src/Details.js
@@ -0,0 +1,176 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Mohamed Ahmed",
+  tagline: "I build things for the web",
+  img: profile,
+  about: ` I'm a dedicated front-end developer passionate about creating immersive web experiences and aspiring to transition into the world of software engineering. I thrive on the intersection of design and functionality, aiming to craft user-centric solutions that captivate and engage.
+
+  Journey So Far
+  My journey into the tech realm started with a profound interest in web development and its impact on user interactions. Exploring the languages of the web — HTML, CSS, and JavaScript — unveiled an exciting world where creativity meets logic.
+  
+  Constant Learning and Growth
+  Driven by an unquenchable thirst for knowledge, I've been exploring cutting-edge frameworks like React.js and Vue.js to stay abreast of the latest technologies. Through personal projects and collaborative endeavors, I've polished my skills in building responsive, innovative, and user-friendly web solutions.
+  
+  What Inspires Me
+  The prospect of contributing to innovative software engineering solutions fuels my passion. I'm energized by the challenge of solving intricate problems while creating elegant, efficient, and impactful solutions. I firmly believe in technology's power to shape a better future and am eager to contribute my skills to meaningful projects.
+  
+  Beyond Coding
+  When I'm not immersed in code, I'm absorbing tech articles, refining design principles, and embracing problem-solving challenges. Collaboration is a value I hold dear, appreciating the synergy of teamwork and open communication to achieve exceptional results.
+
+  What's Next
+  Currently, I'm actively pursuing a deeper understanding of computer science concepts and honing my skills in algorithmic problem-solving. I'm enthusiastic about joining a dynamic team, leveraging my expertise, and learning from seasoned professionals to make valuable contributions.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://github.com/mothepro12?tab=repositories",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/mohamedalaaaa1/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web developer intern",
+    Company: `LavaLoon`,
+    Location: "egypt",
+    Type: "Full Time",
+    Duration: "Sep 2020 - Dec 202021",
+  },
+ 
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "software engineering student",
+    university: "western governers university",
+    Location: "new york ",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "high school student",
+    Company: `e.b.s`,
+    Location: "egypt",
+    Type: "Full Time",
+    Duration: "Aug 2014 - 2017",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Chat App",
+    image: projectImage1,
+    description: `this is a real live chat app`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://poetic-macaron-332112.netlify.app/",
+    githubLink: "https://github.com/mothepro12/chat-app/tree/master",
+    
+  },
+  {
+    title: "Crypto Data",
+    image: projectImage2,
+    description: `simple crypto tracker`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://glittery-bombolone-af12d7.netlify.app/",
+    githubLink: "https://github.com/mothepro12/crypto-geeks/tree/masterr",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "mohamedalaazx@gmail.com",
+  phone: "3472726879",
+};
